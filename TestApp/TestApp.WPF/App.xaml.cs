e6e87046323b6aa32350e0b7e5d5e537c14a6f2b
Nilay Barot <38154678+nullbyte01@users.noreply.github.com>
2021-12-07 12:44:44 +0530

    IntialBase Commit

---
+++ TestApp/TestApp.WPF/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestApp.WPF.Services;
+
+namespace TestApp.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly IHost _host;
+        static App()
+        {
+            _host = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                services.AddHttpClient<ITestService, TestService>(client =>
+                 {
+                     client.BaseAddress = new Uri("https://localhost:7226");
+                 });
+                services.AddSingleton<MainWindow>();
+            }).Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            MainWindow = _host.Services.GetService<MainWindow>();
+
+            if (MainWindow != null)
+                MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ TestApp/TestApp.WPF/Services/TestService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.WPF.Services
+{
+    public class TestService : ITestService
+    {
+        private readonly HttpClient _httpClient;
+        public TestService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetData()
+        {
+            string result = await _httpClient.GetStringAsync("/api/all");
+            return result;
+        }
+    }
+}
